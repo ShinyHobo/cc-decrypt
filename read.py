2dd25d6f556f0185c65d44e4f4d5a5d323b35cd9
ShinyHobo <shinyhobo1@gmail.com>
2024-04-12 12:54:41 -0500

    Added more characters Updated data splitter, added possible hash, client info, and eye color positions

--- read.py
+++ read.py
@@ -1,12 +1,13 @@
 import os
 
 script_dir = os.path.dirname(__file__)
-relpath1 = "Characters/Obi_Wan_Kenobi.chf"
+relpath1 = "Characters/Default_Male.chf" # default
 filepath1 = os.path.join(script_dir, relpath1)
 data = open(filepath1, 'rb').read()
 
-relpath2 = "Characters/Walter_White.chf"
-#relpath2 = "Characters/jSchlatt.chf"
+#relpath2 = "Characters/Freckle_49.chf" # freckle offset 1
+#relpath2 = "Characters/Green_Eyes.chf" # green eyes
+relpath2 = "Characters/Blue_Eyes.chf" # blue eyes
 filepath2 = os.path.join(script_dir, relpath2)
 data2 = open(filepath2, 'rb').read()
 
@@ -33,38 +34,17 @@ for idx, i in enumerate(data.hex(' ').split(' ')):
             starting_idx = idx
             somethingCount+=1
 
-signature = data[:4] # unchanged, always 42 42 00 00
+signature = data[:4].hex(' ') # unchanged, always 42 42 00 00
 
-something2 = data[4:9] # changed
-something3 = data[9:23] # unchanged
-something4 = data[23:24] # changed
-something5 = data[24:26] # unchanged
-something6 = data[26:29] # changed
-something7 = data[29:31] # unchanged
-something8 = data[31:90] # changed
-something9 = data[90:91] # unchanged
-something10 = data[91:92] # changed
-something11 = data[92:93] # unchanged
-something12 = data[93:94] # changed
-something13 = data[94:95] # unchanged
-something14 = data[95:188] # changed
-something15 = data[188:189] # unchanged
-something16 = data[189:320] # changed
-something17 = data[320:321] # unchanged
-something18 = data[321:388] # changed
-something19 = data[388:389] # unchanged
-something20 = data[389:524] # changed
-something21 = data[524:525] # unchanged
-something22 = data[525:542] # changed
-something23 = data[542:543] # unchanged
-something24 = data[543:620] # changed
-something25 = data[620:621] # unchanged
-something26 = data[621:1014] # changed
+hash = data[4:9].hex(' ') # changed, always changes? possible hash
+possible_client_info = data[9:23].hex(' ') # unchanged, never changes for same user - identifying info?
+
+eye_color = data[783:786].hex(' ') # RGB values
 
 # variable length, find first instance TODO
-deadbeef = data[1014:4089] # unchanged
+deadbeef = data[1014:4089].hex(' ') # unchanged
 
-eof = data[4089:4091] # changed, always 3 bytes?
+eof = data[4089:4091].hex(' ') # changed, always 3 bytes?
 
 # DEADBEEF padding to reach 4k file size
 
