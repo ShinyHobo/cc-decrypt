9e3422fc2841910d2ed840bf52a27f5c7b438389
ShinyHobo <shinyhobo1@gmail.com>
2024-04-12 15:48:38 -0500

    added freckle note

--- read.py
+++ read.py
@@ -4,7 +4,7 @@ import re
 script_dir = os.path.dirname(__file__)
 #first_file = "Characters/M_Default.chf" # default male
 #first_file = "Characters/F_Default.chf" # default female
-first_file = "Characters/F_Eyes_Brown.chf"
+first_file = "Characters/M_Freckle_49.chf"
 filepath1 = os.path.join(script_dir, first_file)
 data = open(filepath1, 'rb').read()
 data_hex = data.hex().upper()
@@ -56,7 +56,10 @@ something6 = data[23:25].hex(' ')
 identifier2 = data[25:26].hex(' ') # always unchanged - 00
 
 # eye color always seems to be between these two values; possibly adds 3 extra bits to end, ie FF FF FF 00 00 00
+# TODO - does not work with M_Freckle_49.chf
 eye_color = re.search("C4369700AC342A44(.*)584D4227", data_hex).group(1)
+
+# common pattern after eye color - 584D4227
 
 # variable length, find first instance TODO
 deadbeef = data[1014:4089].hex(' ') # unchanged
